8319e0a729a5ff92e57a402b18016d6d21820c4c
Lukasz Wicher <lukasz.wicher25@gmail.com>
2014-03-20 22:25:27 +0100

    Second change.

--- LogoInterpreter/src/testowa.java
+++ LogoInterpreter/src/testowa.java
@@ -6,7 +6,8 @@ public class testowa {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("test");
+		System.out.print("testowy");
+		System.out.print("testowy");
 	}
 
 }
